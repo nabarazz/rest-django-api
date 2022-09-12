96de0529d520e7ca76f8a9fa3ebf8ad4cd9d19ef
nabarazz <nabarazz01@outlook.com>
2022-09-12 15:56:06 +0545

    a

--- server/taxi/urls.py
+++ server/taxi/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
     path('api/log_in/', LogInView.as_view(), name='log_in'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/trip/', include('trips.urls')),
+    path('api/', include('trips.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
